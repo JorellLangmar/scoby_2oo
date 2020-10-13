8668a55e76a61676a8e9b35c8068b55b67031d6a
Marcha <marchaf@gmail.com>
2020-10-13 10:29:27 +0200

    bug fixes

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
-
+const uploader = require("../config/cloudinary");
+const bcrypt = require("bcrypt");
 
 router.get("/", function (req, res, next) {
   res.send("respond with a resource");
@@ -11,13 +12,20 @@ router.get("/", function (req, res, next) {
 ///api/users
 
 router.patch('/me', uploader.single("profileImg"), async function(req, res, next) {
-  console.log(req.body);
+  console.log(req.body.password);
   console.log(req.session.currentUser._id); 
+  const salt = 10;
   try {
-    const updateUser = req.body;
+    let updateUser = req.body;
     if (req.file) {
       updateUser.profileImg = req.file.path;
       }
+
+      if(req.body.password){
+        const hashedPassword = bcrypt.hashSync(req.body.password, salt);
+        updateUser = {...updateUser, hashedPassword}
+      }
+      
     
     const dbResponse = await User.findByIdAndUpdate(req.session.currentUser._id, updateUser, {new : true});
     res.status(201)
